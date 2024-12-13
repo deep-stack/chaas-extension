96686cfa27cfe17c78ce1cc9bfaf4d04d9b98cdb
Shreerang Kale <shreerangkale@gmail.com>
2024-12-13 10:56:32 +0530

    Update extension styles

--- extension/inject.js
+++ extension/inject.js
@@ -13,11 +13,11 @@ const CHATBOT_URL = 'https://chaas.site';
   iframe.style.bottom = '20px';
   iframe.style.right = '20px';
   iframe.style.zIndex = '999999';
-  iframe.style.width = '170px';
+  iframe.style.width = '168px';
   iframe.style.height = '42px';
   iframe.style.border = 'none';
   iframe.style.display = 'none';
-  iframe.style.borderRadius = '20px';
+  iframe.style.borderRadius = '11px';
   iframe.style.boxShadow = '0 4px 10px rgba(0, 0, 0, 0.1)';
   iframe.style.overflow = 'hidden';
   iframe.style.transition = 'width 0.3s ease, height 0.3s ease';
@@ -32,14 +32,13 @@ const CHATBOT_URL = 'https://chaas.site';
 
       iframe.contentWindow.postMessage(
         { type: 'PARENT_URL', url: currentURL },
-        CHATBOT_URL
+        CHATBOT_URL,
       );
     }
 
     if (event.data.type === 'CHATBOT_STYLES') {
-      iframe.style.width = event.data.isChatOpen ? '40vw' : '170px';
+      iframe.style.width = event.data.isChatOpen ? '800px' : '168px';
       iframe.style.height = event.data.isChatOpen ? '70vh' : '42px';
-      iframe.style.borderRadius = event.data.isChatOpen ? '10px': '20px';
       iframe.style.display = 'block';
     }
   });
